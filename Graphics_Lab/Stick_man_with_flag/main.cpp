849484e889259f87a876931b40cbbd810ae3ca5f
Towfiq379 <36331420+Towfiq379@users.noreply.github.com>
2020-01-02 17:15:36 +0600

    Update main.cpp

--- Graphics_Lab/Stick_man_with_flag/main.cpp
+++ Graphics_Lab/Stick_man_with_flag/main.cpp
@@ -2,6 +2,18 @@
 #include <GL/glut.h>
 using namespace std;
 
+void circle(int h, int k, int r, double R, double G, double B,int flag)
+{
+    if(flag)
+        glBegin(GL_POLYGON);
+    else
+        glBegin(GL_LINE_LOOP);
+    glColor3d(R, G, B);
+        for(double i = 0; i < 2 * acos(-1); i += 0.1)
+            glVertex2d(h + r * cos(i), k + r * sin(i));
+    glEnd();
+}
+
 void Flag()
 {
     glBegin(GL_POLYGON);
@@ -12,11 +24,7 @@ void Flag()
         glVertex2d(70, 280);
     glEnd();
 
-    glBegin(GL_POLYGON);
-    glColor3d(1, .0, 0);
-        for(double i = 0; i < 2 * acos(-1); i += 0.1)
-            glVertex2d(160 + 40 * cos(i), 340 + 40 * sin(i));
-    glEnd();
+    circle(160, 340, 40, 1, 0, 0, 1);
 
     glBegin(GL_POLYGON);
     glColor3d(1, 1, 0);
@@ -30,6 +38,7 @@ void Flag()
     glColor3d(1, 1, 1);
         glVertex2d(65, 390);
         glVertex2d(70, 390);
+
         glVertex2d(65, 290);
         glVertex2d(70, 290);
     glEnd();
@@ -84,7 +93,6 @@ void display()
     glFlush();
     //glutSwapBuffers();
 }
-
 
 int main(int argc, char *argv[])
 {
